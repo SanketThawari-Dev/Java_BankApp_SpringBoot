65389b4a5020b77f916a64f66104b98b37467bef
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-16 21:13:28 +0530

    Update Launch6Controller.java

--- src/main/java/com/bankapp/controller/Launch6Controller.java
+++ src/main/java/com/bankapp/controller/Launch6Controller.java
@@ -2,13 +2,15 @@ package com.bankapp.controller;
 
 import java.sql.Connection;
 
+import javax.sql.DataSource;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.bankapp.config.ConnectionFactory;
 import com.bankapp.dao.AccountDao;
 import com.bankapp.dao.Transac_his_dao;
 import com.bankapp.entity.Account;
@@ -16,7 +18,8 @@ import com.bankapp.entity.Account;
 @Controller
 public class Launch6Controller {
 
-    Connection con = ConnectionFactory.getCon();
+    @Autowired
+    private DataSource dataSource;
 
     AccountDao acDao = new AccountDao();
     Transac_his_dao txnDao = new Transac_his_dao();
@@ -24,23 +27,32 @@ public class Launch6Controller {
     @PostMapping("/transfer")
     public String transfer(HttpServletRequest req) {
 
-        String uid = req.getParameter("uid");
-        String am = req.getParameter("am");
-        String acb = req.getParameter("acb");
-
-        String res = acDao.mt(con, uid, am, acb);
-        txnDao.insertTrans(con, uid, "money transfer", am);
-
-        Account account = acDao.readAccount(con, uid);
         HttpSession session = req.getSession();
 
-        if ("updated".equals(res)) {
-            session.setAttribute("check", uid);
-            session.setAttribute("ac", account);
-            session.setAttribute("msg", "Money Transfered");
-            return "redirect:/account.jsp";
-        } else {
-            session.setAttribute("msg", "Insufficient funds");
+        try (Connection con = dataSource.getConnection()) {
+
+            String uid = req.getParameter("uid");
+            String am = req.getParameter("am");
+            String acb = req.getParameter("acb");
+
+            String res = acDao.mt(con, uid, am, acb);
+            txnDao.insertTrans(con, uid, "money transfer", am);
+
+            Account account = acDao.readAccount(con, uid);
+
+            if ("updated".equals(res)) {
+                session.setAttribute("check", uid);
+                session.setAttribute("ac", account);
+                session.setAttribute("msg", "Money Transferred");
+                return "redirect:/account.jsp";
+            } else {
+                session.setAttribute("msg", "Insufficient funds");
+                return "redirect:/account.jsp";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.setAttribute("msg", "Internal server error");
             return "redirect:/account.jsp";
         }
     }
