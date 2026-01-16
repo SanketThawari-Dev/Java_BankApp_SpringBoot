7e06c58c54c543c29886c3a9facba580688e4c73
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-16 18:06:58 +0530

    Add Launch10Controller for account redirection

---
+++ src/main/java/com/bankapp/controller/Launch10Controller.java
@@ -0,0 +1,17 @@
+package com.bankapp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Launch10Controller {
+
+    @GetMapping("/accountu")
+    public String accountu(HttpServletRequest req) {
+
+        req.getSession().removeAttribute("msg");
+        return "redirect:/account.jsp";
+    }
+}
