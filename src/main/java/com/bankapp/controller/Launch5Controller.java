a205b38f42b182c69ad5beec438ab3acb83231a8
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-16 21:12:42 +0530

    Update Launch5Controller.java

--- src/main/java/com/bankapp/controller/Launch5Controller.java
+++ src/main/java/com/bankapp/controller/Launch5Controller.java
@@ -2,13 +2,15 @@ package com.bankapp.controller;
 
 import java.sql.Connection;
 
+import javax.sql.DataSource;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.bankapp.config.ConnectionFactory;
 import com.bankapp.dao.AccountDao;
 import com.bankapp.dao.Transac_his_dao;
 import com.bankapp.entity.Account;
@@ -16,7 +18,8 @@ import com.bankapp.entity.Account;
 @Controller
 public class Launch5Controller {
 
-    Connection con = ConnectionFactory.getCon();
+    @Autowired
+    private DataSource dataSource;
 
     AccountDao acDao = new AccountDao();
     Transac_his_dao txnDao = new Transac_his_dao();
@@ -24,22 +27,31 @@ public class Launch5Controller {
     @PostMapping("/withdraw")
     public String withdraw(HttpServletRequest req) {
 
-        String uid = req.getParameter("uid");
-        String am = req.getParameter("am");
-
-        String res = acDao.withdraw(con, uid, am);
-        txnDao.insertTrans(con, uid, "withdraw", am);
-
-        Account account = acDao.readAccount(con, uid);
         HttpSession session = req.getSession();
 
-        if ("updated".equals(res)) {
-            session.setAttribute("check", uid);
-            session.setAttribute("ac", account);
-            session.setAttribute("msg", "Money withdraw");
-            return "redirect:/account.jsp";
-        } else {
-            session.setAttribute("msg", "Insufficient funds");
+        try (Connection con = dataSource.getConnection()) {
+
+            String uid = req.getParameter("uid");
+            String am = req.getParameter("am");
+
+            String res = acDao.withdraw(con, uid, am);
+            txnDao.insertTrans(con, uid, "withdraw", am);
+
+            Account account = acDao.readAccount(con, uid);
+
+            if ("updated".equals(res)) {
+                session.setAttribute("check", uid);
+                session.setAttribute("ac", account);
+                session.setAttribute("msg", "Money withdraw");
+                return "redirect:/account.jsp";
+            } else {
+                session.setAttribute("msg", "Insufficient funds");
+                return "redirect:/account.jsp";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.setAttribute("msg", "Internal server error");
             return "redirect:/account.jsp";
         }
     }
