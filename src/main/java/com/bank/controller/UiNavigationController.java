2d267aaae49124f6c97e2f108b3c00c9b65f80fa
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-27 16:36:03 +0530

    Update UiNavigationController.java

--- src/main/java/com/bank/controller/UiNavigationController.java
+++ src/main/java/com/bank/controller/UiNavigationController.java
@@ -1,22 +1,31 @@
+package com.bank.controller;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class UiNavigationController {
 
+    // Launch8.java
     @GetMapping("/loginu")
     public String loginu(HttpSession session) {
         session.removeAttribute("msg");
         return "redirect:/login.jsp";
     }
 
+    // Launch9.java
     @GetMapping("/signupu")
     public String signupu(HttpSession session) {
         session.removeAttribute("msg");
         return "redirect:/signup.jsp";
     }
 
+    // Launch10.java
     @GetMapping("/accountu")
     public String accountu(HttpSession session) {
         session.removeAttribute("msg");
         return "redirect:/account.jsp";
     }
 }
-
