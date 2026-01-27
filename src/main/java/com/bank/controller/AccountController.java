5cfebf0cde98e781e69090d0dff435ef2592abe1
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-27 16:35:19 +0530

    Update AccountController.java

--- src/main/java/com/bank/controller/AccountController.java
+++ src/main/java/com/bank/controller/AccountController.java
@@ -1,52 +1,84 @@
+package com.bank.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bank.service.AccountService;
+import com.bank.service.TransactionService;
+
 @Controller
 public class AccountController {
 
     private final AccountService accountService;
-    private final TransactionService txnService;
+    private final TransactionService transactionService;
 
-    public AccountController(AccountService accountService, TransactionService txnService) {
+    public AccountController(AccountService accountService,
+                             TransactionService transactionService) {
         this.accountService = accountService;
-        this.txnService = txnService;
+        this.transactionService = transactionService;
     }
 
+    // Launch4.java
     @PostMapping("/deposit")
     public String deposit(HttpServletRequest req, HttpSession session) {
+
         String uid = req.getParameter("uid");
         String am = req.getParameter("am");
 
         String res = accountService.deposit(uid, am);
-        txnService.insertTxn(uid, "deposit", am);
+        transactionService.insertTxn(uid, "deposit", am);
 
         session.setAttribute("ac", accountService.readAccount(uid));
-        session.setAttribute("msg", res.equals("updated") ? "Money deposited" : "Failed");
+
+        session.setAttribute(
+                "msg",
+                "updated".equals(res) ? "Money deposited" : "Failed"
+        );
+
         return "redirect:/account.jsp";
     }
 
+    // Launch5.java
     @PostMapping("/withdraw")
     public String withdraw(HttpServletRequest req, HttpSession session) {
+
         String uid = req.getParameter("uid");
         String am = req.getParameter("am");
 
         String res = accountService.withdraw(uid, am);
-        txnService.insertTxn(uid, "withdraw", am);
+        transactionService.insertTxn(uid, "withdraw", am);
 
         session.setAttribute("ac", accountService.readAccount(uid));
-        session.setAttribute("msg", res.equals("updated") ? "Money withdraw" : "Insufficient funds");
+
+        session.setAttribute(
+                "msg",
+                "updated".equals(res) ? "Money withdraw" : "Insufficient funds"
+        );
+
         return "redirect:/account.jsp";
     }
 
+    // Launch6.java
     @PostMapping("/transfer")
     public String transfer(HttpServletRequest req, HttpSession session) {
+
         String uid = req.getParameter("uid");
         String am = req.getParameter("am");
         String acb = req.getParameter("acb");
 
         String res = accountService.transfer(uid, am, acb);
-        txnService.insertTxn(uid, "money transfer", am);
+        transactionService.insertTxn(uid, "money transfer", am);
 
         session.setAttribute("ac", accountService.readAccount(uid));
-        session.setAttribute("msg", res.equals("updated") ? "Money Transfered" : "Insufficient funds");
+
+        session.setAttribute(
+                "msg",
+                "updated".equals(res) ? "Money Transfered" : "Insufficient funds"
+        );
+
         return "redirect:/account.jsp";
     }
 }
-
