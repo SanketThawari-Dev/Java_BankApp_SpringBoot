2a7be8fffd30d132de93f4d2a7fbc05feb122707
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-27 16:22:57 +0530

    Update AuthController.java

--- src/main/java/com/bank/controller/AuthController.java
+++ src/main/java/com/bank/controller/AuthController.java
@@ -1,31 +1,47 @@
+package com.bank.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bank.service.AccountService;
+import com.bank.service.TransactionService;
+
 @Controller
 public class AuthController {
 
     private final AccountService accountService;
-    private final TransactionService txnService;
+    private final TransactionService transactionService;
 
-    public AuthController(AccountService accountService, TransactionService txnService) {
+    public AuthController(AccountService accountService,
+                          TransactionService transactionService) {
         this.accountService = accountService;
-        this.txnService = txnService;
+        this.transactionService = transactionService;
     }
 
     @PostMapping("/signup")
     public String signup(HttpServletRequest req, HttpSession session) {
-        String uid = req.getParameter("uid");
 
-        txnService.createTxnTable(uid);
+        String uid = req.getParameter("uid");
+        transactionService.createTxnTable(uid);
+
         String res = accountService.insert(req);
 
         if ("inserted".equals(res)) {
             session.setAttribute("msg", "Account created");
             return "redirect:/login.jsp";
         }
+
         session.setAttribute("msg", "Account Creation Failed");
         return "redirect:/signup.jsp";
     }
 
     @PostMapping("/login")
     public String login(HttpServletRequest req, HttpSession session) {
+
         session.removeAttribute("msg");
 
         String uid = req.getParameter("uid");
@@ -36,6 +52,7 @@ public class AuthController {
             session.setAttribute("ac", accountService.readAccount(uid));
             return "redirect:/account.jsp";
         }
+
         session.setAttribute("msg", "Invalid Userid or password");
         return "redirect:/login.jsp";
     }
@@ -46,4 +63,3 @@ public class AuthController {
         return "redirect:/login.jsp";
     }
 }
-
