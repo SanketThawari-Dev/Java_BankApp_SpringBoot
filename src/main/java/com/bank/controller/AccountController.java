0d7f4c91cba18e85ad415e6eb3359b815c7669cc
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-27 16:03:31 +0530

    Update AccountController.java

--- src/main/java/com/bank/controller/AccountController.java
+++ src/main/java/com/bank/controller/AccountController.java
@@ -1,1 +1,52 @@
+@Controller
+public class AccountController {
+
+    private final AccountService accountService;
+    private final TransactionService txnService;
+
+    public AccountController(AccountService accountService, TransactionService txnService) {
+        this.accountService = accountService;
+        this.txnService = txnService;
+    }
+
+    @PostMapping("/deposit")
+    public String deposit(HttpServletRequest req, HttpSession session) {
+        String uid = req.getParameter("uid");
+        String am = req.getParameter("am");
+
+        String res = accountService.deposit(uid, am);
+        txnService.insertTxn(uid, "deposit", am);
+
+        session.setAttribute("ac", accountService.readAccount(uid));
+        session.setAttribute("msg", res.equals("updated") ? "Money deposited" : "Failed");
+        return "redirect:/account.jsp";
+    }
+
+    @PostMapping("/withdraw")
+    public String withdraw(HttpServletRequest req, HttpSession session) {
+        String uid = req.getParameter("uid");
+        String am = req.getParameter("am");
+
+        String res = accountService.withdraw(uid, am);
+        txnService.insertTxn(uid, "withdraw", am);
+
+        session.setAttribute("ac", accountService.readAccount(uid));
+        session.setAttribute("msg", res.equals("updated") ? "Money withdraw" : "Insufficient funds");
+        return "redirect:/account.jsp";
+    }
+
+    @PostMapping("/transfer")
+    public String transfer(HttpServletRequest req, HttpSession session) {
+        String uid = req.getParameter("uid");
+        String am = req.getParameter("am");
+        String acb = req.getParameter("acb");
+
+        String res = accountService.transfer(uid, am, acb);
+        txnService.insertTxn(uid, "money transfer", am);
+
+        session.setAttribute("ac", accountService.readAccount(uid));
+        session.setAttribute("msg", res.equals("updated") ? "Money Transfered" : "Insufficient funds");
+        return "redirect:/account.jsp";
+    }
+}
 
