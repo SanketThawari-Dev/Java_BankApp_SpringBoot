8416895af887aba0440fb9b72c02f97dc79ce74d
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-27 16:03:09 +0530

    Update AuthController.java

--- src/main/java/com/bank/controller/AuthController.java
+++ src/main/java/com/bank/controller/AuthController.java
@@ -1,1 +1,49 @@
+@Controller
+public class AuthController {
+
+    private final AccountService accountService;
+    private final TransactionService txnService;
+
+    public AuthController(AccountService accountService, TransactionService txnService) {
+        this.accountService = accountService;
+        this.txnService = txnService;
+    }
+
+    @PostMapping("/signup")
+    public String signup(HttpServletRequest req, HttpSession session) {
+        String uid = req.getParameter("uid");
+
+        txnService.createTxnTable(uid);
+        String res = accountService.insert(req);
+
+        if ("inserted".equals(res)) {
+            session.setAttribute("msg", "Account created");
+            return "redirect:/login.jsp";
+        }
+        session.setAttribute("msg", "Account Creation Failed");
+        return "redirect:/signup.jsp";
+    }
+
+    @PostMapping("/login")
+    public String login(HttpServletRequest req, HttpSession session) {
+        session.removeAttribute("msg");
+
+        String uid = req.getParameter("uid");
+        String upass = req.getParameter("upass");
+
+        if ("exits".equals(accountService.checkUser(uid, upass))) {
+            session.setAttribute("check", uid);
+            session.setAttribute("ac", accountService.readAccount(uid));
+            return "redirect:/account.jsp";
+        }
+        session.setAttribute("msg", "Invalid Userid or password");
+        return "redirect:/login.jsp";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("check");
+        return "redirect:/login.jsp";
+    }
+}
 
